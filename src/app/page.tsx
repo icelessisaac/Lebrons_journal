9c04026a5acd30a7cb887f17fbe108115064a49b
bruce <aaaeeettt124@126.com>
2024-11-30 21:35:45 +0800

    fixed safety api url issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@
 import Image from "next/image";
 import { ConnectButton, useActiveAccount } from "thirdweb/react";
 import thirdwebIcon from "@public/thirdweb.svg";
-import { client } from "./client";
+import { client } from "@/server/client";
 
 import { ActiveAcc } from "@/components/activeAcc";
 
