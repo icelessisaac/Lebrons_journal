62af6b167a3f9959123222c6603fec65b5dc9bc2
kevin <13621755931@163.com>
2024-12-04 18:23:44 +0800

    fixing div problem and moduling Header tsx

--- src/app/message/page.tsx
+++ src/app/message/page.tsx
@@ -1,7 +1,4 @@
 "use client";
-import thirdwebIcon from "@public/thirdweb.svg";
-import Image from "next/image";
-
 import { ConnectButton, useActiveAccount } from "thirdweb/react";
 
 import { client } from "@/server";
@@ -10,6 +7,7 @@ import { myChain } from "@/server/contracts/message";
 // 导入 SendMessage 和 ReceiveMessage 组件
 import { SendMessage } from "@/components/message/sendMessage";
 import { ReceiveMessage } from "@/components/message/receiveMessage";
+import { Header } from "@/components/header";
 
 export default function Message() {
   // 使用 useActiveAccount 钩子来检查用户是否已连接钱包
@@ -46,34 +44,5 @@ export default function Message() {
         </div>
       </div>
     </main>
-  );
-}
-
-function Header() {
-  return (
-    <header className="flex flex-col items-center mb-20 md:mb-20">
-      <Image
-        src={thirdwebIcon}
-        alt=""
-        className="size-[150px] md:size-[150px]"
-        style={{
-          filter: "drop-shadow(0px 0px 24px #a726a9a8)",
-        }}
-      />
-
-      <h1 className="text-2xl md:text-6xl font-semibold md:font-bold tracking-tighter mb-6 text-zinc-100">
-        DIAO
-        <span className="text-zinc-300 inline-block mx-1"> + </span>
-        <span className="inline-block -skew-x-6 text-blue-500"> CHAT </span>
-      </h1>
-
-      <p className="text-zinc-300 text-base">
-        The true
-        <code className="bg-zinc-800 text-zinc-300 px-2 rounded py-1 text-sm mx-1">
-          DAPP
-        </code>{" "}
-        on EVM.
-      </p>
-    </header>
   );
 }
