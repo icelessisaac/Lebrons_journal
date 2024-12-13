3f2c7b9c7d1725f0927628886f5d4c96bd080fb0
bruce <aaaeeettt124@126.com>
2024-12-13 16:28:33 +0800

    metadelet

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,5 @@
-import Metadata from "next";
-
 import "./globals.css";
 import { ThirdwebProvider } from "thirdweb/react";
-
-export const metadata: Metadata = {
-  title: "DIAOCHAT",
-  description:
-    "Starter template for using thirdweb SDK with Next.js App router",
-};
 
 export default function RootLayout({
   children,
